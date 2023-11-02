b823473bd85a360f2719f4d1bc1766dff197bd60
updateyuan <864570604@qq.com>
2023-11-02 21:04:30 +0800

    Site updated: 2023-11-02 21:04:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2022/10/31/hello-world/","2023/10/31/hello-world1/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/31/hello-world1/","2022/10/31/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
